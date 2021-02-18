0abb05d036ea8b0fa85905d8740869b99135909d
DustinPark92 <p4569zz@gmail.com>
2021-02-18 18:08:49 +0900

    configure MemoModel

---
+++ RxMemo/Model/Memo.swift
@@ -0,0 +1,31 @@
+//
+//  Memo.swift
+//  RxMemo
+//
+//  Created by Dustin on 2021/02/18.
+//
+
+import Foundation
+
+
+struct Memo : Equatable {
+    var content : String
+    var insertDate : Date
+    var identify : String
+    
+    init(content : String , insertDate : Date = Date()) {
+        self.content = content
+        self.insertDate = insertDate
+        self.identify = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    
+    
+    //새로운 메모를 생성
+    init(original : Memo , updatedContent: String) {
+        self = original
+        self.content = updatedContent
+        
+    }
+    
+}
