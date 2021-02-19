c6f2e32b5837620da6e67230ffe1cccb53ead2f8
DustinPark92 <p4569zz@gmail.com>
2021-02-19 16:47:21 +0900

    memoComposeViewController

--- RxMemo/ViewController/MemoComposeViewController.swift
+++ RxMemo/ViewController/MemoComposeViewController.swift
@@ -6,19 +6,61 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Action
+import NSObject_Rx
 
 class MemoComposeViewController: UIViewController,viewModelBindableType {
     
     var viewModel : MemoComposeViewModel!
-
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    @IBOutlet weak var contentTextView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
        
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        contentTextView.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if contentTextView.isFirstResponder {
+            contentTextView.resignFirstResponder()
+        }
+        
+        
+    }
+    
 
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx_disposeBag)
+        
+        
+        viewModel.initialText
+            .drive(contentTextView.rx.text)
+            .disposed(by: rx_disposeBag)
+        
+        
+        cancelButton.rx.action = viewModel.cancelAction
+        
+        saveButton.rx.tap
+            .throttle(.microseconds(500), scheduler: MainScheduler.instance)
+            .withLatestFrom(contentTextView.rx.text.orEmpty)
+            .bind(to: viewModel.saveAction.inputs)
+            .disposed(by: rx_disposeBag)
         
     }
 
--- RxMemo/ViewModel/MemoComposeViewModel.swift
+++ RxMemo/ViewModel/MemoComposeViewModel.swift
@@ -10,21 +10,29 @@ import RxSwift
 import RxCocoa
 import Action
 
-
-class MemoComposeViewModel : CommonViewModel {
-    private let content : String?
+class MemoComposeViewModel: CommonViewModel {
+    // 메모를 저장하는 속성
+    private let content: String?
     
-    
-    var initialText : Driver<String?> {
+    // 뷰에 바인딩할 수 있는 드라이버 추가.
+    var initialText: Driver<String?> {
         return Observable.just(content).asDriver(onErrorJustReturn: nil)
     }
     
-    let saveAction : Action<String, Void>
-    let cancelAction : CocoaAction
+    // 저장과 취소 두 가지 액션을 구현함
     
+    // 액션을 저장하는 속성
+    let saveAction: Action<String, Void>
+    let cancelAction: CocoaAction
     
-    init(title : String, content : String? = nil , sceneCoordinator : SceneCoordinatorType, storage : MemoStorageType, saveAction : Action<String,Void>? = nil , cancelAction : CocoaAction? = nil ) {
+    // 뷰 모델에서 취소코드와 저장코드를 직접 구현하지 않고 파라미터로 받는다.
+    // 이렇게 하면 처리방식이 하나로 고정되지않고 이전 화면에서 처리방식을 동적으로 결정할 수 있다.
+    init(title: String, content: String? = nil, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType, saveAction: Action<String, Void>?, cancelAction: CocoaAction? = nil) {
+        
         self.content = content
+        
+        // saveAction으로 전달된 액션을 액션으로 한 번더 맵핑하여 실제로 전달됐으면 실행하고 화면닫고
+        // 아니면 화면만 닫고 끝낸다.
         self.saveAction = Action<String, Void> { input in
             if let action = saveAction {
                 action.execute(input)
