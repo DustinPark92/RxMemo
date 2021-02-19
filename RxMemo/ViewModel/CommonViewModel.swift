48115929947aabc5f94dd7e798db602a2f700645
DustinPark92 <p4569zz@gmail.com>
2021-02-19 16:06:46 +0900

    memoList

---
+++ RxMemo/ViewModel/CommonViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  CommonViewModel.swift
+//  RxMemo
+//
+//  Created by Dustin on 2021/02/19.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+
+
+class CommonViewModel : NSObject {
+    // 네비게이션 타이틀을 위한 드라이버
+    
+    let title : Driver<String>
+    
+    
+    //Scene coordinator와 저장소를 저장하는 속성 선언
+    
+    let sceneCoordinator : SceneCoordinatorType
+    let storage : MemoStorageType
+    //protocol로 선언 의존성을 쉽게 선언 할 수 있다.
+    
+    init(title : String,sceneCoordinator : SceneCoordinatorType , storage : MemoStorageType ) {
+        self.title = Observable.just(title).asDriver(onErrorRecover: "")
+        self.sceneCoordinator = sceneCoordinator
+        self.storage = storage
+    }
+    
+    
+    
+    
+    
+}
--- RxMemo/ViewModel/MemoListViewModel.swift
+++ RxMemo/ViewModel/MemoListViewModel.swift
@@ -6,9 +6,19 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+// 뷰 모델 추가되는 것
+// 1. 의존성을 주입하는 생성자.
+// 2. 바인딩에 사용되는 프로퍼티와 메소드
 
 
-class MemoListViewModel {
+class MemoListViewModel : CommonViewModel {
+    
+    var memoList : Observable<[Memo]> {
+        return storage.memoList()
+    }
     
     
     
