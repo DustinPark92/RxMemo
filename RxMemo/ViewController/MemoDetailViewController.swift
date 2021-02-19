f72fbbefc1584d82978a46156c3020f9c3262c9a
DustinPark92 <p4569zz@gmail.com>
2021-02-19 17:10:49 +0900

    MemoDetailViewModel

--- RxMemo/ViewController/MemoDetailViewController.swift
+++ RxMemo/ViewController/MemoDetailViewController.swift
@@ -10,17 +10,48 @@ import UIKit
 class MemoDetailViewController: UIViewController,viewModelBindableType {
     
     var viewModel : MemoDetailViewModel!
-
+    
+    @IBOutlet weak var listTableView: UITableView!
+    
+    @IBOutlet weak var deleteButton: UIBarButtonItem!
+    
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
     
-
-
+    
+    
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx_disposeBag)
         
+        viewModel.contents
+            .bind(to: listTableView.rx.items) {
+                (tableView , row , value) in
+                
+                
+                switch row {
+                case 0 :
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "contentCell")!
+                    cell.textLabel?.text = value
+                    return cell
+                case 1 :
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell")!
+                    cell.textLabel?.text = value
+                    return cell
+                default:
+                    fatalError()
+                }
+                
+            }.disposed(by: rx_disposeBag)
     }
-
+    
 }
